1319427476a69891ffb3ebf9f3e67ecd47bb5026
Jonathan Channon <jonathan@jonathanchannon.com>
2012-11-14 21:29:08 +0000

    Initial commit

---
+++ MVCF1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCF1.Models;
+using RestSharp;
+
+namespace MVCF1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var client = new RestClient("http://ergast.com");
+
+            var request = new RestRequest("api/f1/" + DateTime.Now.Year + "/driverStandings.json", Method.GET);
+
+            request.AddHeader("Accept", "application/json");
+
+            var response = client.Execute<RootObject>(request);
+
+            RootObject model = null;
+
+            if (!string.IsNullOrWhiteSpace(response.ErrorMessage) || response.Data == null)
+                model = new RootObject();
+            else
+                model = response.Data;
+
+
+
+            return View(model);
+        }
+
+    }
+}
---
+++ MVCF1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCF1.MvcApplication" Language="C#" %>
